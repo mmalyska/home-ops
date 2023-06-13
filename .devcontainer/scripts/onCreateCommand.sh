852094f1be99dd384d43c4fadbcee2b51d032298
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2023-06-13 13:31:07 +0200

    add ignore for k8s dashboard

--- .devcontainer/scripts/onCreateCommand.sh
+++ .devcontainer/scripts/onCreateCommand.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -i
-sudo git config --system --add safe.directory "${1}"
+sudo git config --global --add safe.directory "${1}"
+sudo git config --global gpg.program gpg
 
 direnv allow
 
