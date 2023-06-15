3e1885894a95a9ded5ea3e6154bdc282bebcc013
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2023-06-15 13:19:04 +0000

    fix zone name

--- provision/terraform/cloudflare/firewall.tf
+++ provision/terraform/cloudflare/firewall.tf
@@ -1,11 +1,11 @@
 # Block Countries
 resource "cloudflare_filter" "block_countries" {
-  zone_id     = data.cloudflare_zone.domain.id
+  zone_id     = data.cloudflare_settings.domain.id
   description = "Expression to block countries"
   expression  = "(ip.geoip.country in {\"CN\" \"IN\" \"RU\"})"
 }
 resource "cloudflare_firewall_rule" "block_countries" {
-  zone_id     = data.cloudflare_zone.domain.id
+  zone_id     = data.cloudflare_settings.domain.id
   description = "Firewall rule to block countries"
   filter_id   = cloudflare_filter.block_countries.id
   action      = "block"
@@ -13,12 +13,12 @@ resource "cloudflare_firewall_rule" "block_countries" {
 
 # Block Bots
 resource "cloudflare_filter" "bots" {
-  zone_id     = data.cloudflare_zone.domain.id
+  zone_id     = data.cloudflare_settings.domain.id
   description = "Expression to block bots determined by CF"
   expression  = "(cf.client.bot) or (cf.threat_score gt 14)"
 }
 resource "cloudflare_firewall_rule" "bots" {
-  zone_id     = data.cloudflare_zone.domain.id
+  zone_id     = data.cloudflare_settings.domain.id
   description = "Firewall rule to block bots determined by CF"
   filter_id   = cloudflare_filter.bots.id
   action      = "block"
@@ -44,7 +44,7 @@ resource "cloudflare_list" "uptimerobot" {
   }
 }
 resource "cloudflare_filter" "uptimerobot" {
-  zone_id     = data.cloudflare_zone.domain.id
+  zone_id     = data.cloudflare_settings.domain.id
   description = "Expression to allow UptimeRobot IP addresses"
   expression  = "(ip.src in $uptimerobot)"
   depends_on = [
@@ -52,7 +52,7 @@ resource "cloudflare_filter" "uptimerobot" {
   ]
 }
 resource "cloudflare_firewall_rule" "uptimerobot" {
-  zone_id     = data.cloudflare_zone.domain.id
+  zone_id     = data.cloudflare_settings.domain.id
   description = "Firewall rule to allow UptimeRobot IP addresses"
   filter_id   = cloudflare_filter.uptimerobot.id
   action      = "allow"
