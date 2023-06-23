5ad4856ea6f67ba7d1806580944478f18ec43e39
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2023-06-23 14:30:43 +0000

    add rules to tunnel

--- provision/terraform/cloudflare/tunel.tf
+++ provision/terraform/cloudflare/tunel.tf
@@ -12,3 +12,30 @@ resource "doppler_secret" "cloudflare_tunnel" {
   name = "CLOUDFLARE_TUNNEL_TOKEN"
   value = cloudflare_tunnel.jaskinia.tunnel_token
 }
+
+resource "cloudflare_record" "ingress" {
+  name    = "ingress"
+  zone_id = data.cloudflare_zone.domain.id
+  value   = "${cloudflare_tunnel.jaskinia.id}.cfargotunnel.com"
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
+
+resource "cloudflare_tunnel_config" "jaskinia_config" {
+  account_id = cloudflare_account.main.id
+  tunnel_id  = cloudflare_tunnel.jaskinia.id
+
+  config {
+    ingress_rule {
+      hostname = "*.${local.cloudflare_domain}"
+      service  = "https://traefik.traefik.svc.cluster.local:443"
+      origin_request {
+        origin_server_name = cloudflare_record.ingress.hostname
+      }
+    }
+    ingress_rule {
+      service = "http_status:404"
+    }
+  }
+}
