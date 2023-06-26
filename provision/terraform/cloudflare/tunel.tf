4073cea07f8ae4defb212bb35352207d1a580d98
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2023-06-26 12:18:06 +0000

    update tunnel

--- provision/terraform/cloudflare/tunel.tf
+++ provision/terraform/cloudflare/tunel.tf
@@ -28,6 +28,13 @@ resource "cloudflare_tunnel_config" "jaskinia_config" {
 
   config {
     ingress_rule {
+      hostname = "hass.${local.cloudflare_domain}"
+      service  = "http://web-service.httpbin.svc.cluster.local:80"
+      origin_request {
+        origin_server_name = cloudflare_record.ingress.hostname
+      }
+    }
+    ingress_rule {
       hostname = "*.${local.cloudflare_domain}"
       service  = "https://traefik.traefik.svc.cluster.local:443"
       origin_request {
