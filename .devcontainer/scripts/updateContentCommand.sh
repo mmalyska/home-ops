5cf27192139da9fa918dbe5860e187b4cd22b1e1
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2023-07-07 12:08:40 +0000

    test tf token

--- .devcontainer/scripts/updateContentCommand.sh
+++ .devcontainer/scripts/updateContentCommand.sh
@@ -1,21 +1,4 @@
 #!/bin/bash -i
-if [ "$SYNC_LOCALHOST_CONFIGS" = "true" ]; then
-    mkdir -p "$HOME"/.kube
-    sudo cp -r /usr/local/share/kube-localhost/* "$HOME"/.kube
-    sudo chown -R "$(id -u)" "$HOME"/.kube
-
-    mkdir -p "$HOME"/.config/sops
-    sudo cp -r /usr/local/share/sops-localhost/* "$HOME"/.config/sops
-    sudo chown -R "$(id -u)" "$HOME"/.config/sops
-
-    mkdir -p "$HOME"/.ssh
-    sudo cp -r /usr/local/share/ssh-localhost/* "$HOME"/.ssh
-    sudo chown -R "$(id -u)" "$HOME"/.ssh
-    sudo chmod 755 "$HOME"/.ssh
-    sudo chmod 600 "$HOME"/.ssh/*
-    sudo chmod 644 "$HOME"/.ssh/known_hosts
-fi
-
 git gc --prune=now
 brew update
 brew upgrade
