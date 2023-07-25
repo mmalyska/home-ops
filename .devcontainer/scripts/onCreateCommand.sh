0d97c323095f54afa4145c9953847bb8a2708dd5
Michał <999598+mmalyska@users.noreply.github.com>
2023-07-25 12:32:48 +0000

    add pull.rebase

--- .devcontainer/scripts/onCreateCommand.sh
+++ .devcontainer/scripts/onCreateCommand.sh
@@ -2,6 +2,8 @@
 sudo git config --global --add safe.directory "${1}"
 sudo git config --global gpg.program gpg
 sudo git config --global commit.gpgsign true
+sudo git config --global user.signingkey F6F676C60A077962
+sudo git config --global pull.rebase true
 
 direnv allow
 
