09aecc816746776b9e547acba9f0013f4dfa54b3
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2024-03-27 15:17:39 +0000

    move apt

--- .github/.devcontainer/local-features/homebrew/install.sh
+++ .github/.devcontainer/local-features/homebrew/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -i
 
+apt-get update
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y install build-essential procps curl file git --no-install-recommends
+apt-get clean -y
+rm -rf /var/lib/apt/lists/*
+
 su "$_REMOTE_USER" <<EOF
   set -exm
   ./install_hb.sh
--- .github/.devcontainer/local-features/homebrew/install_hb.sh
+++ .github/.devcontainer/local-features/homebrew/install_hb.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -i
 
-apt-get update
-export DEBIAN_FRONTEND=noninteractive
-apt-get -y install build-essential procps curl file git --no-install-recommends
-apt-get clean -y
-rm -rf /var/lib/apt/lists/*
-
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 case "${SHELL}" in
