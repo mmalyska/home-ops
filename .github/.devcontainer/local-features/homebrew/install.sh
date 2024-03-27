6c7cf144ffb4b241ea3148e5d63db5dda834898b
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2024-03-27 21:43:07 +0000

    t7

--- .github/.devcontainer/local-features/homebrew/install.sh
+++ .github/.devcontainer/local-features/homebrew/install.sh
@@ -8,19 +8,4 @@ rm -rf /var/lib/apt/lists/*
 
 sudo -u ${_REMOTE_USER} ./install_hb.sh
 
-case "${SHELL}" in
-  */bash*)
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "${HOME}/.bashrc"
-    ;;
-  */zsh*)
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "${ZDOTDIR:-"${HOME}"}/.zshrc"
-    ;;
-  */fish*)
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "${HOME}/.config/fish/config.fish"
-    ;;
-  *)
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "${ENV:-"${HOME}/.profile"}"
-    ;;
-esac
-
-sudo -u ${_REMOTE_USER} "cat ${HOME}/.bashrc && brew config"
+sudo -u ${_REMOTE_USER} brew config
--- .github/.devcontainer/local-features/homebrew/install_hb.sh
+++ .github/.devcontainer/local-features/homebrew/install_hb.sh
@@ -2,19 +2,5 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-case "${SHELL}" in
-  */bash*)
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ${HOME}/.bashrc
-    ;;
-  */zsh*)
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "${ZDOTDIR:-"${HOME}"}/.zshrc"
-    ;;
-  */fish*)
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "${HOME}/.config/fish/config.fish"
-    ;;
-  *)
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "${ENV:-"${HOME}/.profile"}"
-    ;;
-esac
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew analytics off
