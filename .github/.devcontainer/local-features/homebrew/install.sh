8802c677e71ee8c68010f47abb569faa62e404a2
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2024-03-27 21:28:03 +0000

    t7

--- .github/.devcontainer/local-features/homebrew/install.sh
+++ .github/.devcontainer/local-features/homebrew/install.sh
@@ -6,7 +6,7 @@ apt-get -y install build-essential procps curl file git --no-install-recommends
 apt-get clean -y
 rm -rf /var/lib/apt/lists/*
 
-su - ${_REMOTE_USER} -c "./install_hb.sh"
+sudo -u ${_REMOTE_USER} ./install_hb.sh
 
 case "${SHELL}" in
   */bash*)
@@ -23,4 +23,4 @@ case "${SHELL}" in
     ;;
 esac
 
-su - ${_REMOTE_USER} -c "cat ${HOME}/.bashrc && brew config"
+sudo -u ${_REMOTE_USER} "brew config"
