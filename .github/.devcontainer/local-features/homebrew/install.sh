b913cb63299d8ad6f5e3f3f72df056d54f3ea3c0
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2024-03-27 13:12:32 +0000

    fix hb install

--- .github/.devcontainer/local-features/homebrew/install.sh
+++ .github/.devcontainer/local-features/homebrew/install.sh
@@ -4,31 +4,4 @@ apt-get update && export DEBIAN_FRONTEND=noninteractive \
 && apt-get -y install build-essential procps curl file git --no-install-recommends \
 && apt-get clean -y && rm -rf /var/lib/apt/lists/*
 
-case "${SHELL}" in
-  */bash*)
-    if [[ -n "${HOMEBREW_ON_LINUX-}" ]]
-    then
-      shell_rcfile="${HOME}/.bashrc"
-    else
-      shell_rcfile="${HOME}/.bash_profile"
-    fi
-    ;;
-  */zsh*)
-    if [[ -n "${HOMEBREW_ON_LINUX-}" ]]
-    then
-      shell_rcfile="${ZDOTDIR:-"${HOME}"}/.zshrc"
-    else
-      shell_rcfile="${ZDOTDIR:-"${HOME}"}/.zprofile"
-    fi
-    ;;
-  */fish*)
-    shell_rcfile="${HOME}/.config/fish/config.fish"
-    ;;
-  *)
-    shell_rcfile="${ENV:-"${HOME}/.profile"}"
-    ;;
-esac
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" \
-&& eval "$(${HOMEBREW_PREFIX}/bin/brew shellenv)" >> ${shell_rcfile} \
-&& brew analytics off
+sudo -u $_REMOTE_USER ./install_hb.sh
