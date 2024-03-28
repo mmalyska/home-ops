fb22a65e6f981f402a319207fe7fc9b041733cd7
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2024-03-28 14:56:14 +0000

    fix brew path

--- .github/.devcontainer/local-features/common/install_user.sh
+++ .github/.devcontainer/local-features/common/install_user.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -i
 
-brew install age ansible direnv gitleaks go-task/tap/go-task helm ipcalc jq kubectl kustomize prettier sops stern terraform yamllint yq argocd int128/kubelogin/kubelogin ansible-lint k9s pre-commit mkdocs
+/home/linuxbrew/.linuxbrew/bin/brew install age ansible direnv gitleaks go-task/tap/go-task helm ipcalc jq kubectl kustomize prettier sops stern terraform yamllint yq argocd int128/kubelogin/kubelogin ansible-lint k9s pre-commit mkdocs
