42da98b8de64b0fb0db8e6ea0debc24d9810abf8
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2024-03-28 14:30:19 +0000

    add test config

--- .github/.devcontainer/local-features/common/install_user.sh
+++ .github/.devcontainer/local-features/common/install_user.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -i
 
+echo $PATH
+
+/home/linuxbrew/.linuxbrew/bin/brew config
+
 brew install age ansible direnv gitleaks go-task/tap/go-task helm ipcalc jq kubectl kustomize prettier sops stern terraform yamllint yq argocd int128/kubelogin/kubelogin ansible-lint k9s pre-commit mkdocs
