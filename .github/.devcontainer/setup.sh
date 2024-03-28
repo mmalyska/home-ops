ea6704bca895641a720355d9fc1baa086d5d69aa
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2024-03-28 11:56:42 +0000

    test command runs

--- .github/.devcontainer/setup.sh
+++ .github/.devcontainer/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -i
 sudo git config --global --add safe.directory "${1}"
-sudo git config --global gpg.program gpg
-sudo git config --global commit.gpgsign true
-sudo git config --global user.signingkey F6F676C60A077962
 sudo git config --global pull.rebase true
 
 echo 'eval "$(direnv hook zsh)"' >> /home/vscode/.zshrc
@@ -12,4 +9,4 @@ direnv allow
 
 task init
 
-echo "Done!"
+echo "Done init!"
