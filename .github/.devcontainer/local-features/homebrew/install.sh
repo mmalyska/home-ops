40d0d58ab79d900d848682dd10f78a61f1bf0079
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2024-03-28 08:33:12 +0000

    fix options order

--- .github/.devcontainer/local-features/homebrew/install.sh
+++ .github/.devcontainer/local-features/homebrew/install.sh
@@ -6,4 +6,4 @@ apt-get -y install build-essential procps curl file git --no-install-recommends
 apt-get clean -y
 rm -rf /var/lib/apt/lists/*
 
-sudo -Hun ${_REMOTE_USER} ./install_hb.sh
+sudo -Hn -u ${_REMOTE_USER} ./install_hb.sh
