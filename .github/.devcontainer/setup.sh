76e579bef6ff2d0abb314ea7d682e5509dce5d25
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2024-03-28 12:12:47 +0000

    add command execution

--- .github/.devcontainer/setup.sh
+++ .github/.devcontainer/setup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -i
-sudo git config --global --add safe.directory "${1}"
-sudo git config --global pull.rebase true
-
 echo 'eval "$(direnv hook zsh)"' >> /home/vscode/.zshrc
 echo 'eval "$(direnv hook bash)"' >> /home/vscode/.bashrc
 
