0fce78e35c413dd1805d617669f912af55d5953e
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2024-03-28 14:08:36 +0000

    add common feature

---
+++ .github/.devcontainer/local-features/common/install_user.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -i
+
+brew install age ansible direnv gitleaks go-task/tap/go-task helm ipcalc jq kubectl kustomize prettier sops stern terraform yamllint yq argocd int128/kubelogin/kubelogin ansible-lint k9s pre-commit mkdocs
