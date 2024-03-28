03f8eff57421fc312c4f6483d5458736d907384b
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2024-03-28 15:03:25 +0000

    cleanups

--- .devcontainer/scripts/updateContentCommand.sh
+++ .devcontainer/scripts/updateContentCommand.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -i
 git gc --prune=now
-brew update
-brew upgrade
 
 KUBECONFIG=/home/vscode/.kube/config
 
--- .github/.devcontainer/local-features/common/install_user.sh
+++ .github/.devcontainer/local-features/common/install_user.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -i
 
 /home/linuxbrew/.linuxbrew/bin/brew install age ansible direnv gitleaks go-task/tap/go-task helm ipcalc jq kubectl kustomize prettier sops stern terraform yamllint yq argocd int128/kubelogin/kubelogin ansible-lint k9s pre-commit mkdocs
+
+echo 'eval "$(direnv hook zsh)"' >> /home/vscode/.zshrc
+echo 'eval "$(direnv hook bash)"' >> /home/vscode/.bashrc
