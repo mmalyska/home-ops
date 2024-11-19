5db20a06d7c3d0975044c5ffe8c1fa48d37d9401
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2024-11-19 11:07:34 +0000

    sudo tar

--- .github/.devcontainer/local-features/sops-replacer/install_user.sh
+++ .github/.devcontainer/local-features/sops-replacer/install_user.sh
@@ -5,8 +5,8 @@ VERSION=0.3.0
 ARCH=$(dpkg --print-architecture)
 
 case "${ARCH}" in \
-'amd64') curl -sL https://github.com/mmalyska/argocd-secret-replacer/releases/download/v${VERSION}/secret-replacer-v${VERSION}-linux-x64.tar.gz | tar -xvz --no-same-owner; ;; \
-'arm64') curl -sL https://github.com/mmalyska/argocd-secret-replacer/releases/download/v${VERSION}/secret-replacer-v${VERSION}-linux-arm64.tar.gz | tar -xvz --no-same-owner; ;; \
+'amd64') curl -sL https://github.com/mmalyska/argocd-secret-replacer/releases/download/v${VERSION}/secret-replacer-v${VERSION}-linux-x64.tar.gz | sudo tar -xvz ; ;; \
+'arm64') curl -sL https://github.com/mmalyska/argocd-secret-replacer/releases/download/v${VERSION}/secret-replacer-v${VERSION}-linux-arm64.tar.gz | sudo tar -xvz; ;; \
 esac && \
 mv argocd-secret-replacer /usr/bin/argocd-secret-replacer && \
 chmod +x /usr/bin/argocd-secret-replacer
