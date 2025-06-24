5bd2d571e9863e3152f639c5aa4f46d3f0a71805
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2025-06-24 20:42:37 +0000

    disable uptime robot

--- provision/terraform/cloudflare/firewall.tf
+++ provision/terraform/cloudflare/firewall.tf
@@ -1,24 +1,24 @@
-data "http" "uptimerobot_ips" {
-  url = "https://uptimerobot.com/inc/files/ips/IPv4.txt"
-}
+# data "http" "uptimerobot_ips" {
+#   url = "https://uptimerobot.com/inc/files/ips/IPv4.txt"
+# }
 
-locals {
-  uptime_ips = split("\n", data.http.uptimerobot_ips.response_body)
-}
+# locals {
+#   uptime_ips = split("\n", data.http.uptimerobot_ips.response_body)
+# }
 
-resource "cloudflare_list" "uptimerobot" {
-  account_id  = cloudflare_account.main.id
-  name        = "uptimerobot"
-  kind        = "ip"
-  description = "List of UptimeRobot IP Addresses"
-}
+# resource "cloudflare_list" "uptimerobot" {
+#   account_id  = cloudflare_account.main.id
+#   name        = "uptimerobot"
+#   kind        = "ip"
+#   description = "List of UptimeRobot IP Addresses"
+# }
 
-resource "cloudflare_list_item" "example" {
-  account_id = cloudflare_account.main.id
-  list_id = cloudflare_list.uptimerobot.id
-  for_each = toset(local.uptime_ips)
-  ip = each.value
-}
+# resource "cloudflare_list_item" "uptimerobot" {
+#   account_id = cloudflare_account.main.id
+#   list_id = cloudflare_list.uptimerobot.id
+#   for_each = toset(local.uptime_ips)
+#   ip = each.value
+# }
 
 resource "cloudflare_ruleset" "zone_level_custom_waf" {
   zone_id     = cloudflare_zone.domain.id
@@ -26,23 +26,24 @@ resource "cloudflare_ruleset" "zone_level_custom_waf" {
   description = "Zone-level WAF Custom Rules config"
   kind        = "zone"
   phase       = "http_request_firewall_custom"
-  depends_on  = [
-    cloudflare_list.uptimerobot,
-  ]
+  # depends_on  = [
+  #   cloudflare_list.uptimerobot,
+  # ]
 
   # Accept UptimeRobot Addresses
-  rules = [{
-    action      = "skip"
-    action_parameters = {
-      ruleset = "current"
-    }
-    expression  = "(ip.src in $uptimerobot)"
-    description = "Expression to allow UptimeRobot IP addresses"
-    enabled     = true
-    logging = {
-      enabled   = true
-    }
-  },
+  rules = [
+  # {
+  #   action      = "skip"
+  #   action_parameters = {
+  #     ruleset = "current"
+  #   }
+  #   expression  = "(ip.src in $uptimerobot)"
+  #   description = "Expression to allow UptimeRobot IP addresses"
+  #   enabled     = true
+  #   logging = {
+  #     enabled   = true
+  #   }
+  # },
   # Block Countries
   {
     action      = "block"
