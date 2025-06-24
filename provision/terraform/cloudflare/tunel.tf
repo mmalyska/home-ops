02c557ee1860473be5234b00b3a69fd980f5f509
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2025-06-24 21:31:26 +0200

    migrate records (#2903)

--- provision/terraform/cloudflare/tunel.tf
+++ provision/terraform/cloudflare/tunel.tf
@@ -13,15 +13,6 @@ resource "doppler_secret" "cloudflare_tunnel" {
   value = cloudflare_zero_trust_tunnel_cloudflared.jaskinia.tunnel_token
 }
 
-resource "cloudflare_record" "ingress" {
-  name    = "ingress"
-  zone_id = data.cloudflare_zone.domain.id
-  content   = "${cloudflare_zero_trust_tunnel_cloudflared.jaskinia.id}.cfargotunnel.com"
-  proxied = true
-  type    = "CNAME"
-  ttl     = 1
-}
-
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "jaskinia_config" {
   account_id = cloudflare_account.main.id
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.jaskinia.id
@@ -30,16 +21,10 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "jaskinia_config" {
     ingress_rule {
       hostname = "${local.cloudflare_domain}"
       service  = "https://traefik.traefik.svc.cluster.local:443"
-      origin_request {
-        origin_server_name = cloudflare_record.ingress.hostname
-      }
     }
     ingress_rule {
       hostname = "*.${local.cloudflare_domain}"
       service  = "https://traefik.traefik.svc.cluster.local:443"
-      origin_request {
-        origin_server_name = cloudflare_record.ingress.hostname
-      }
     }
     ingress_rule {
       service = "http_status:404"
