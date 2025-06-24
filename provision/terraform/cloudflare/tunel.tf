242167842f40c4f81e48c2313961b0aa2d9c1863
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2025-06-24 19:46:08 +0000

    remove resources

--- provision/terraform/cloudflare/tunel.tf
+++ provision/terraform/cloudflare/tunel.tf
@@ -12,22 +12,3 @@ resource "doppler_secret" "cloudflare_tunnel" {
   name = "CLOUDFLARE_TUNNEL_TOKEN"
   value = cloudflare_zero_trust_tunnel_cloudflared.jaskinia.tunnel_token
 }
-
-resource "cloudflare_zero_trust_tunnel_cloudflared_config" "jaskinia_config" {
-  account_id = cloudflare_account.main.id
-  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.jaskinia.id
-
-  config {
-    ingress_rule {
-      hostname = "${local.cloudflare_domain}"
-      service  = "https://traefik.traefik.svc.cluster.local:443"
-    }
-    ingress_rule {
-      hostname = "*.${local.cloudflare_domain}"
-      service  = "https://traefik.traefik.svc.cluster.local:443"
-    }
-    ingress_rule {
-      service = "http_status:404"
-    }
-  }
-}
