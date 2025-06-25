bb42c7450f6c987068ef36ef2e834f5873860b71
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2025-06-25 07:55:36 +0000

    add records

--- provision/terraform/cloudflare/records.tf
+++ provision/terraform/cloudflare/records.tf
@@ -1,61 +1,61 @@
-# resource "cloudflare_dns_record" "root" {
-#   name    = local.cloudflare_domain
-#   zone_id = cloudflare_zone.domain.id
-#   content   = local.cloudflare_ingress_hostname
-#   proxied = true
-#   type    = "CNAME"
-#   ttl     = 1
-# }
+resource "cloudflare_dns_record" "root" {
+  name    = local.cloudflare_domain
+  zone_id = cloudflare_zone.domain.id
+  content   = loudflare_dns_record.ingress.name
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
 
-# resource "cloudflare_dns_record" "grocy" {
-#   name    = "grocy"
-#   zone_id = cloudflare_zone.domain.id
-#   content = local.cloudflare_ingress_hostname
-#   proxied = true
-#   type    = "CNAME"
-#   ttl     = 1
-# }
+resource "cloudflare_dns_record" "grocy" {
+  name    = "grocy.${local.cloudflare_domain}"
+  zone_id = cloudflare_zone.domain.id
+  content = loudflare_dns_record.ingress.name
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
 
-# resource "cloudflare_dns_record" "hass" {
-#   name    = "hass"
-#   zone_id = cloudflare_zone.domain.id
-#   content = local.cloudflare_ingress_hostname
-#   proxied = true
-#   type    = "CNAME"
-#   ttl     = 1
-# }
+resource "cloudflare_dns_record" "hass" {
+  name    = "hass.${local.cloudflare_domain}"
+  zone_id = cloudflare_zone.domain.id
+  content = loudflare_dns_record.ingress.name
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
 
-# resource "cloudflare_dns_record" "l" {
-#   name    = "l"
-#   zone_id = cloudflare_zone.domain.id
-#   content = local.cloudflare_ingress_hostname
-#   proxied = true
-#   type    = "CNAME"
-#   ttl     = 1
-# }
+resource "cloudflare_dns_record" "l" {
+  name    = "l.${local.cloudflare_domain}"
+  zone_id = cloudflare_zone.domain.id
+  content = loudflare_dns_record.ingress.name
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
 
-# resource "cloudflare_dns_record" "oauth" {
-#   name    = "oauth"
-#   zone_id = cloudflare_zone.domain.id
-#   content = local.cloudflare_ingress_hostname
-#   proxied = true
-#   type    = "CNAME"
-#   ttl     = 1
-# }
+resource "cloudflare_dns_record" "oauth" {
+  name    = "oauth.${local.cloudflare_domain}"
+  zone_id = cloudflare_zone.domain.id
+  content = loudflare_dns_record.ingress.name
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
 
-# resource "cloudflare_dns_record" "doppler" {
-#   name    = local.doppler_domain
-#   zone_id = cloudflare_zone.domain.id
-#   content = local.doppler_value
-#   type    = "TXT"
-#   ttl     = 1
-# }
+resource "cloudflare_dns_record" "doppler" {
+  name    = local.doppler_domain
+  zone_id = cloudflare_zone.domain.id
+  content = local.doppler_value
+  type    = "TXT"
+  ttl     = 1
+}
 
-# resource "cloudflare_dns_record" "chat" {
-#   name    = "chat"
-#   zone_id = cloudflare_zone.domain.id
-#   content = local.cloudflare_ingress_hostname
-#   proxied = true
-#   type    = "CNAME"
-#   ttl     = 1
-# }
+resource "cloudflare_dns_record" "chat" {
+  name    = "chat.${local.cloudflare_domain}"
+  zone_id = cloudflare_zone.domain.id
+  content = loudflare_dns_record.ingress.name
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
