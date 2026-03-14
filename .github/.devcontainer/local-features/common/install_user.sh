33d74d5e32617ac3e12d016bb4d79a819168a8d8
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2026-03-14 19:47:37 +0000

    migrate gitea

--- .github/.devcontainer/local-features/common/install_user.sh
+++ .github/.devcontainer/local-features/common/install_user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -i
 
-/home/linuxbrew/.linuxbrew/bin/brew install age direnv egctl gitleaks go-task/tap/go-task helm ipcalc jq kubectl kustomize prettier sops stern terraform yamllint yq argocd int128/kubelogin/kubelogin k9s pre-commit mkdocs
+/home/linuxbrew/.linuxbrew/bin/brew install age direnv egctl gitleaks go-task/tap/go-task helm ipcalc jq kubectl kustomize prettier sops stern terraform yamllint yq argocd int128/kubelogin/kubelogin k9s pre-commit mkdocs gh claude-code
 
 echo 'eval "$(direnv hook zsh)"' >> /home/vscode/.zshrc
 echo 'eval "$(direnv hook bash)"' >> /home/vscode/.bashrc
