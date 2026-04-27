f3cf3f8697ea33716f8eab3e7098c6afc37ec871
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2026-04-27 12:52:56 +0200

    install as root

--- .github/.devcontainer/local-features/bws-cli/install.sh
+++ .github/.devcontainer/local-features/bws-cli/install.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-sudo -Hn -u ${_REMOTE_USER} ./install_user.sh
+
+# renovate: datasource=github-releases depName=bitwarden/sdk-sm
+VERSION=bws-v2.0.0
+ARCH=$(dpkg --print-architecture)
+
+case "${ARCH}" in \
+'amd64') curl -sL https://github.com/bitwarden/sdk-sm/releases/download/${VERSION}/bws-x86_64-unknown-linux-gnu-${VERSION#bws-v}.zip | funzip > /usr/bin/bws; ;; \
+'arm64') curl -sL https://github.com/bitwarden/sdk-sm/releases/download/${VERSION}/bws-aarch64-unknown-linux-gnu-${VERSION#bws-v}.zip | funzip > /usr/bin/bws; ;; \
+esac && \
+chmod +x /usr/bin/bws
--- .github/.devcontainer/local-features/sops-replacer/install.sh
+++ .github/.devcontainer/local-features/sops-replacer/install.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-sudo -Hn -u ${_REMOTE_USER} ./install_user.sh
+
+# renovate: datasource=github-releases depName=mmalyska/argocd-secret-replacer
+VERSION=0.3.0
+ARCH=$(dpkg --print-architecture)
+
+case "${ARCH}" in \
+'amd64') curl -sL https://github.com/mmalyska/argocd-secret-replacer/releases/download/v${VERSION}/secret-replacer-v${VERSION}-linux-x64.tar.gz | tar -oxvz ; ;; \
+'arm64') curl -sL https://github.com/mmalyska/argocd-secret-replacer/releases/download/v${VERSION}/secret-replacer-v${VERSION}-linux-arm64.tar.gz | tar -oxvz; ;; \
+esac && \
+mv argocd-secret-replacer /usr/bin/argocd-secret-replacer && \
+chmod +x /usr/bin/argocd-secret-replacer
