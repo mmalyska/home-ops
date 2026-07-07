b4468f3fb080e93c83fec5ce312f27c777ff0a3b
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2026-07-07 10:22:56 +0000

    feat(brew): add taps for go-task, hashicorp, and int128/kubelogin in Brewfile and install_user.sh

--- .github/.devcontainer/local-features/common/install_user.sh
+++ .github/.devcontainer/local-features/common/install_user.sh
@@ -3,6 +3,9 @@
 export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
 
 /home/linuxbrew/.linuxbrew/bin/brew update
+/home/linuxbrew/.linuxbrew/bin/brew trust go-task/tap
+/home/linuxbrew/.linuxbrew/bin/brew trust hashicorp/tap
+/home/linuxbrew/.linuxbrew/bin/brew trust int128/kubelogin
 /home/linuxbrew/.linuxbrew/bin/brew bundle install --file=Brewfile && /home/linuxbrew/.linuxbrew/bin/brew bundle upgrade --file=Brewfile
 
 echo 'eval "$(direnv hook zsh)"' >> /home/vscode/.zshrc
