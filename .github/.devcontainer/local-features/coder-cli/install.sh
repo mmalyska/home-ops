6611bc63a2ec85952fe1e45fa25b878008d4ce0b
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2026-07-07 10:02:58 +0000

    feat(coder-cli): add Coder CLI installation scripts and feature definition

---
+++ .github/.devcontainer/local-features/coder-cli/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# renovate: datasource=github-releases depName=coder/coder
+VERSION=v2.33.11
+ARCH=$(dpkg --print-architecture)
+
+curl -sL "https://github.com/coder/coder/releases/download/${VERSION}/coder_${VERSION#v}_linux_${ARCH}.tar.gz" | tar -xzO ./coder > /usr/bin/coder
+chmod +x /usr/bin/coder
