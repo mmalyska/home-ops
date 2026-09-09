735f6907140a6260626d724e7393aa16f24d5158
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2026-09-09 08:27:10 +0000

    fix(Brewfile): change go-task installation from cask to brew and update trust command

--- .github/.devcontainer/local-features/common/install_user.sh
+++ .github/.devcontainer/local-features/common/install_user.sh
@@ -8,7 +8,6 @@ export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PAT
 export XDG_CONFIG_HOME="$HOME/.config"
 
 /home/linuxbrew/.linuxbrew/bin/brew update
-/home/linuxbrew/.linuxbrew/bin/brew trust go-task/tap
 /home/linuxbrew/.linuxbrew/bin/brew trust hashicorp/tap
 /home/linuxbrew/.linuxbrew/bin/brew trust int128/kubelogin
 /home/linuxbrew/.linuxbrew/bin/brew bundle install --file=Brewfile && /home/linuxbrew/.linuxbrew/bin/brew bundle upgrade --file=Brewfile
